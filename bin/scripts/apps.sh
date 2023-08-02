eef243bec7535c4c50246b74e148bb066b5dbdda
AspieSoft <aspiesoftweb@gmail.com>
2023-08-02 01:11:29 -0400

    updated

--- bin/scripts/apps.sh
+++ bin/scripts/apps.sh
@@ -7,7 +7,8 @@ xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 if test -d "$rDir/usr/share/applications"; then
   sudo sed -r -i 's/^OnlyShowIn=/#OnlyShowIn=/m' "/usr/share/applications/nemo.desktop"
   sudo sed -r -i 's#^inode/directory=(.*)$#inode/directory=nemo.desktop#m' "/usr/share/applications/gnome-mimeapps.list"
-  sudo chattr +i "/usr/share/applications/nemo.desktop" # prevent updates from changing this file
+  # sudo chattr +i "/usr/share/applications/nemo.desktop" # prevent updates from changing this file
+  git clone https://github.com/AspieSoft/linux-nemo-fix.git && ./linux-nemo-fix/install.sh
 fi
 
 #todo: install common apps for viewing images, videos, text, etc.
