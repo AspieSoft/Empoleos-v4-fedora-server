3af2dfd3c77e3081f30e237c38eaa4d40b231ad0
AspieSoft <aspiesoftweb@gmail.com>
2023-08-04 18:05:46 -0400

    updated

--- bin/scripts/theme/config.sh
+++ bin/scripts/theme/config.sh
@@ -14,3 +14,5 @@ gsettings set org.gnome.desktop.background picture-uri-dark "file:///usr/share/b
 gsettings set org.gnome.mutter center-new-windows "true"
 gsettings set org.gnome.mutter attach-modal-dialogs "false"
 gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
+
+gsettings set org.gnome.TextEditor restore-session "false"
--- bin/scripts/theme/user-extensions-config.sh
+++ bin/scripts/theme/user-extensions-config.sh
@@ -6,6 +6,3 @@
 mkdir -p "~/.config/burn-my-windows/profiles"
 cp ./bin/assets/extensions/burn-my-windows.conf "~/.config/burn-my-windows/profiles"
 gsettings --schemadir ~/.local/share/gnome-shell/extensions/burn-my-windows@schneegans.github.com/schemas/ set org.gnome.shell.extensions.burn-my-windows active-profile "~/.config/burn-my-windows/profiles/burn-my-windows.conf"
-
-# other config options
-gsettings set org.gnome.TextEditor restore-session "false"
