a8d5e636d15a4c720f9acfe03ce7a7db77a9b686
Shayne Taylor <aspiesoftweb@gmail.com>
2023-08-06 12:55:04 -0400

    Update run.4.sh

--- bin/empoleos-installer/run/run.4.sh
+++ bin/empoleos-installer/run/run.4.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+ServerMode="$1"
+dir="$PWD"
+
 # install auto updates
 if [ "$ServerMode" = "y" ]; then
   sudo sed -r -i 's/^#ServerMode=$/ServerMode=/m' "$dir/bin/apps/empoleos/update.sh"
 fi
-sudo cp -rf "./bin/assets/empoleos" "/etc"
+sudo cp -rf "$dir/bin/assets/empoleos" "/etc"
 sudo rm -f "/etc/empoleos/empoleos.service"
-sudo cp -f "./bin/assets/empoleos/empoleos.service" "/etc/systemd/system"
+sudo cp -f "$dir/bin/assets/empoleos/empoleos.service" "/etc/systemd/system"
 gitVer="$(curl --silent 'https://api.github.com/repos/AspieSoft/Empoleos/releases/latest' | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/')"
 echo "$gitVer" | sudo tee "/etc/empoleos/version.txt"
 
