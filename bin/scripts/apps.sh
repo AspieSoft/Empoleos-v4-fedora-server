5bfca8bc2463453d6f343e2375ad299b7c2137e6
Shayne Taylor <aspiesoftweb@gmail.com>
2023-08-06 13:14:04 -0400

    Update apps.sh

--- bin/scripts/apps.sh
+++ bin/scripts/apps.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+dir="$1"
+
 sudo dnf -y install nemo
 sudo dnf -y install nemo-fileroller
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 
-if test -d "$rDir/usr/share/applications"; then
+if test -d "/usr/share/applications"; then
   sudo sed -r -i 's/^OnlyShowIn=/#OnlyShowIn=/m' "/usr/share/applications/nemo.desktop"
   sudo sed -r -i 's#^inode/directory=(.*)$#inode/directory=nemo.desktop#m' "/usr/share/applications/gnome-mimeapps.list"
   git clone https://github.com/AspieSoft/linux-nemo-fix.git && ./linux-nemo-fix/install.sh
@@ -112,4 +114,4 @@ mkdir "/usr/games/$USER/Steam"
 mv "$HOME/.local/share/Steam/steamapps" "/usr/games/$USER/Steam/steamapps"
 ln -s "/usr/games/$USER/Steam/steamapps" "$HOME/.local/share/Steam/steamapps"
 
-sudo cp "./bin/assets/adduser.local" "/usr/local/sbin"
+sudo cp "$dir/bin/assets/adduser.local" "/usr/local/sbin"
