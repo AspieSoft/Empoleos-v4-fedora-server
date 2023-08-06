9a1170107f6987364bc1317c0efaa8066edddbc9
Shayne Taylor <shaynejr@localhost.localdomain>
2023-08-06 15:38:10 -0400

    updated

--- bin/empoleos-installer/run/run.2.sh
+++ bin/empoleos-installer/run/run.2.sh
@@ -9,7 +9,7 @@ bash "$dir/bin/scripts/theme/core-extensions-config.sh" "$dir"
 # move gnome core extensions to root
 sudo mv $HOME/.local/share/gnome-shell/extensions/* /usr/share/gnome-shell/extensions
 for file in $(ls /usr/share/gnome-shell/extensions); do
-  sudo -R root:root "/usr/share/gnome-shell/extensions/$file"
+  sudo chown -R root:root "/usr/share/gnome-shell/extensions/$file"
 done
 
 bash "$dir/bin/scripts/theme/user-extensions.sh" "$dir"
