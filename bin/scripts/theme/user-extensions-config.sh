b02bf75da4ac57d7f69664598a705fee7726f36d
Shayne Taylor <shaynejr@localhost.localdomain>
2023-08-06 15:45:36 -0400

    updated

--- bin/scripts/theme/user-extensions-config.sh
+++ bin/scripts/theme/user-extensions-config.sh
@@ -5,6 +5,6 @@ dir="$1"
 # config gnome user extension
 
 # - burn my windows
-mkdir -p "~/.config/burn-my-windows/profiles"
-cp "$dir/bin/assets/extensions/burn-my-windows.conf" "~/.config/burn-my-windows/profiles"
+mkdir -p "$HOME/.config/burn-my-windows/profiles"
+cp "$dir/bin/assets/extensions/burn-my-windows.conf" "$HOME/.config/burn-my-windows/profiles"
 gsettings --schemadir ~/.local/share/gnome-shell/extensions/burn-my-windows@schneegans.github.com/schemas/ set org.gnome.shell.extensions.burn-my-windows active-profile "~/.config/burn-my-windows/profiles/burn-my-windows.conf"
